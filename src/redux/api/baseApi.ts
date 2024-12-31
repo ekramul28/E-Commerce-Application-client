e50bde2f609a31e219e9054dd19577b9226431da
Ekramul <mdekramulhassan168@gmail.com>
2024-12-31 14:16:28 +0600

    add base url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -14,8 +14,8 @@ interface ErrorResponse {
 }
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1",
-  // baseUrl: "https://e-commers-server-2.vercel.app/api/v1",
+  // baseUrl: "http://localhost:5000/api/v1",
+  baseUrl: "https://e-commers-server-2.vercel.app/api/v1",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth?.token;
     if (token) {
